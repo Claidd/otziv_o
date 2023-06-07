825ffa77a4a467b1e95a2ebe95764d9336ed09b4
Claidd <iquest138@gmail.com>
2023-06-07 21:36:12 +0300

    1.Start packet

---
+++ src/main/java/com/hunt/otziv/OtzivOApplication.java
@@ -0,0 +1,13 @@
+package com.hunt.otziv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtzivOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtzivOApplication.class, args);
+	}
+
+}
